8ab712152f932bd77bf82abe36a475cf0664242f
Daniel Petersson <daniel.petersson@soprasteria.com>
2020-11-11 11:09:30 +0100

    INTYGFV-13538: New SonarQube at BaseFarm

--- vars/shgradle.groovy
+++ vars/shgradle.groovy
@@ -1,7 +1,8 @@
 def getBuildOpts() {
     return " -DnexusUsername=$NEXUS_USERNAME -DnexusPassword=$NEXUS_PASSWORD" +
         " -DgithubUser=$GITHUB_USERNAME -DgithubPassword=$GITHUB_PASSWORD" +
-        " -DineraNexusUsername=$INERA_NEXUS_USERNAME -DineraNexusPassword=$INERA_NEXUS_PASSWORD"
+        " -DineraNexusUsername=$INERA_NEXUS_USERNAME -DineraNexusPassword=$INERA_NEXUS_PASSWORD" +
+        " -DineraSonarLogin=$INERA_SONAR_LOGIN"
 }
 
 def call(gradleCommand) {
